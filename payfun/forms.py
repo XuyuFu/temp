b0bebec51b3958e60acf046e81a45fbd9d70ea26
Xuyu Fu <xuyuf@andrew.cmu.edu>
2018-03-25 19:38:11 -0400

    post

---
+++ payfun/forms.py
@@ -0,0 +1,54 @@
+from django import forms
+
+import PayFun.models
+
+MAX_UPLOAD_SIZE = 2500000
+
+class LaunchFrom(forms.ModelForm):
+	class Meta:
+        model = Activity
+
+    def clean_content(self):
+    	content = self.cleaned_data['content']
+    	if not content:
+    		raise forms.ValidationError("You have to specify the activity.")
+    	return content 
+
+    def clean_title(self):
+    	title = self.cleaned_data['title']
+    	if not title:
+    		raise forms.ValidationError("You have to name the activity.")
+    	return title
+
+    def clean_brief_description(self):
+    	brief_description = self.cleaned_data['brief_description']
+    	if not brief_description:
+    		raise forms.ValidationError("You have to briefly describe the activity.")
+    	return brief_description
+
+    def clean_target_money(self):
+    	target_money = self.cleaned_data['target_money']
+    	if not target_money:
+    		raise forms.ValidationError("You have to input the amount of money you need.")
+    	return target_money
+
+    def clean_start_time(self):
+		target_money = self.cleaned_data['start_time']
+    	if not start_time:
+    		raise forms.ValidationError("You have to specify the start time of the activity.")
+    	return start_time
+
+    def clean_end_time(self):
+    	end_time = self.cleaned_data['end_time']
+    	if not end_time:
+    		raise forms.ValidationError("You have to specify the end time of the activity.")
+    	return end_time
+
+    def clean_location(self);
+    	location = self.cleaned_data['location']
+    	if not location:
+    		raise forms.ValidationError("You have to specify the location of the activity.")
+    	return location
+
+
+
--- payfun/views.py
+++ payfun/views.py
@@ -1,6 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.utils import timezone
+import time
 
+import PayFun.models
+import PayFun.forms
 # Create your views here.
+def launch():
+	if request.method == 'GET':
+		context = { 'form': LaunchFrom() }
+		return render(request, 'payfun/launch.html', context)
+
+    launch_form = CreateForm(request.POST)
+    if not create_form.is_valid():
+    	context = { 'form': launch_form }
+    	return render(request, 'payfun/launch.html', context)
+
+    lauched_activity = Activity(launcher = request.user, post_time=timezone.now(), 
+    	               is_lauch_success = False, is_hold_success = False, 
+    	               is_start = False)
+    launch_from = LaunchFrom(request.POST, instance = lauched_activity )
+
+    if not launch_form.is_valid():
+    	context = { 'form': launch_form }
+    	return render(request, 'payfun/launch.html', context)
+
+    launch_form.save()
+    message = "You launch an activity"
+    context = { 'message': message, 'activity': lauched_activity, 'form': launch_form }
+    return render(request, 'PayFun/global.html', context)
+
