024fe6d2102a0277494cd15510efef34f6685971
Yuanwei Chen <yuanweic@andrew.cmu.edu>
2018-04-08 16:01:47 -0400

    Added all the models

--- payfun/models.py
+++ payfun/models.py
@@ -4,62 +4,61 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-# class User(models.Model):
-# 	first_name = models.CharField(max_length=100)
-# 	last_name = models.CharField(max_length=100)
-# 	username = models.CharField(max_length=100)
-# 	password = models.CharField(max_length=100)
-# 	phone_number = models.CharField(max_length=100)
-# 	email = models.CharField(max_length=100)
-# 	passPort = models.CharField(max_length=100)
-# 	credits = models.FloatField()
+class User(models.Model):
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	username = models.CharField(max_length=100)
+	password = models.CharField(max_length=100)
+	phone_number = models.CharField(max_length=100)
+	email = models.CharField(max_length=100)
+	# passPort = models.CharField(max_length=100)
+	credits = models.FloatField()
 
-# class Acitivity(models.Model):
-# 	launcher = models.CharField(max_length=100)
-# 	title = models.CharField(max_length=100)
-# 	brief_description = models.CharField(max_length=150)
-# 	content = models.CharField(max_length=1000)
-# 	picture = models.FileField(upload_to="images", blank=True) 
-# 	target_money = models.FloatField() 
-# 	start_time = models.DateTimeField()
-# 	end_time = models.DateTimeField()
-# 	post_time = models.DateTimeField()
-# 	location = models.CharField()
-# 	contributor = models.ForeignKey(User) 
-# 	is_start = models.BinaryField(blank=True) 
-# 	is_lauch_success = models.BinaryField(blank=True) 
-# 	is_hold_success = models.BinaryField(blank=True)
+class Activity(models.Model):
+	#launcher = models.CharField(max_length=100)
+	launcher = models.ForeignKey(User)
+	title = models.CharField(max_length=100)
+	# brief_description = models.CharField(max_length=150)
+	content = models.CharField(max_length=1000)
+	picture = models.FileField(upload_to="images", blank=True)
+	target_money = models.FloatField()
+	start_time = models.DateTimeField()
+	end_time = models.DateTimeField()
+	post_time = models.DateTimeField(auto_now_add=True)
+	location = models.CharField(max_length=100)
+	is_start = models.BooleanField() # whether the activity has started
+	is_end = models.BooleanField() # whether the activity has failed, whether success or failure
+	is_success = models.BooleanField(blank=True)
 
+class LauncherAndActivity(models.Model):
+	launcher = models.ForeignKey(User)
+	activity = models.ForeignKey(Activity)
 
-# class LauncherAndActivity(models.Model):
-# 	launcher = models.ForeignKey(User)
-# 	activity = models.ForeignKey(Activity)
+class SponserAndActivity(models.Model):
+	sponsor = models.ForeignKey(User)
+	activity = models.ForeignKey(Activity)
+	time = models.DateTimeField(auto_now_add=True)
+	money_amount = models.FloatField()
+	is_anonymous = models.BooleanField()
 
-# class SponserAndActivity(models.Model):
-# 	sponsor = models.ForeignKey(User)
-# 	activity = models.ForeignKey(Activity)
-# 	time = models.DateTimeField()
-# 	money_amount = models.FloatField()
-# 	is_anonymous = models.BinaryField()
+class Followed(models.Model):
+ 	user = models.ForeignKey(User)
+ 	activity = models.ForeignKey(Activity)
 
-# class Followed(models.Model):
-# 	user = ForeignKey(User)
-# 	activity = models.ForeignKey(Activity)
+class Progress(models.Model):
+ 	activity = models.ForeignKey(Activity)
+ 	time = models.DateTimeField(auto_now_add=True) # the time at which the progress is made
+ 	content = models.CharField(max_length=150)
+ 	picture = models.FileField(upload_to="images", blank=True)
 
-# class Progress(models.Model):
-# 	activity = models.ForeignKey(Activity)
-# 	time = models.DateTimeField()
-# 	content = models.CharField(max_length=150)
-# 	picture = models.FileField(upload_to="images", blank=True)
+class Comment(models.Model):
+ 	activity = models.ForeignKey(Activity)
+ 	commenter = models.ForeignKey(User)
+ 	comment = models.CharField(max_length=150)
+ 	time = models.DateTimeField(auto_now_add=True)
 
-# class Comment(models.Model):
-# 	activity = models.ForeignKey(Activity)
-# 	commenter = models.ForeignKey(User)
-# 	comment = CharField(max_length=150)
-# 	time = models.DateTimeField()
-
-# class Notification(models.Model):
-# 	activity = models.ForeignKey(Activity)
-# 	sponsor = models.ForeignKey(User)
-# 	notification = CharField(max_length=150)
-# 	time = models.DateTimeField()
+class Notification(models.Model):
+ 	activity = models.ForeignKey(Activity)
+ 	sponsor = models.ForeignKey(User)
+ 	notification = models.CharField(max_length=150)
+ 	time = models.DateTimeField()
--- payfun/views.py
+++ payfun/views.py
@@ -92,3 +92,28 @@ def postsignup(request):
     message = "register successfully"
     return render(request, "login.html", {"messg":message})
 
+# Create your views here.
+@transaction.atomic
+def launch():
+    if request.method == 'GET':
+        context = { 'form': LaunchFrom() }
+        return render(request, 'payfun/launch.html', context)
+
+    launch_form = CreateForm(request.POST)
+    if not create_form.is_valid():
+        context = { 'form': launch_form }
+        return render(request, 'payfun/launch.html', context)
+
+    lauched_activity = Activity(launcher = request.user, post_time=timezone.now(), 
+                       is_lauch_success = False, is_hold_success = False, 
+                       is_start = False)
+    launch_from = LaunchFrom(request.POST, instance = lauched_activity )
+
+    if not launch_form.is_valid():
+        context = { 'form': launch_form }
+        return render(request, 'payfun/launch.html', context)
+
+    launch_form.save()
+    message = "You launch an activity"
+    context = { 'message': message, 'activity': lauched_activity, 'form': launch_form }
+    return render(request, 'PayFun/global.html', context)
